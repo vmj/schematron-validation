8b83d6a7cb09f97bbdc9397eef190d8849beb8e0
Mikko Värri <mikko@varri.fi>
2023-06-17 14:15:18 +0300

    Initial commit

---
+++ src/main/java/fi/linuxbox/schematron/validation/Main.java
@@ -0,0 +1,8 @@
+package fi.linuxbox.schematron.validation;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hello, World! Running on Java "
+                               + System.getProperty("java.version"));
+    }
+}
